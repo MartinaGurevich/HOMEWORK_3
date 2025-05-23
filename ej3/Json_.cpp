036a65d5079925cee15adf134bd8d3d85eb9a875
Martina <martugurevich@hotmail.com>
2025-05-23 19:09:33 +0000

    CORREGIR TODO EL 3

--- ej3/Json_.cpp
+++ ej3/Json_.cpp
@@ -5,16 +5,15 @@
 using namespace std;
 
 //defino tipos de datos 
-using vecDT= vector<double>;
-using PalabrasDT= vector<string>;
-using listasDT=  vector<vector<int>>; 
+// using vecDT= vector<double>;
+using paso_aString= vector<string>;
+// using listasDT=  vector<vector<int>>;
 
 //CLASE 1
 //generic template
 template<typename T> //deberia tener un vector aca que le meta los tipos T , y en l¿clase dos hago agregacion de los vectores 
 class generadora {
 private: 
-
     vector<T> vect_tipo; //vector que se va llenando 
 public:
 
@@ -24,7 +23,7 @@ public:
 
     void construir_Json(const string& etiqueta)const {
         if constexpr(is_same_v<T, vecDT>){
-            cout<<" \""<<etiqueta<<"\" : [";
+            /*cout<<" \""<<etiqueta<<"\*/" :  [";
             for (size_t i=0; i< vect_tipo.size(); i++){
                 cout<<vect_tipo[i];
                 if( i!= vect_tipo.size() -1) {cout << ", ";}
@@ -35,7 +34,7 @@ public:
         }
         }
         else if constexpr(is_same_v<T, PalabrasDT>){
-            cout<<"  \""<<etiqueta<<"\" : [";
+            /*cout<<"  \""<<etiqueta<<"\*/ " : [";
             for (size_t i=0; i< datos.size(); ++i){
                 cout<<"\""<<datos[i]<<"\"";
                 if(i< datos.size()-1){
@@ -47,7 +46,7 @@ public:
         }
         else if constexpr(is_same_v<T, listasDT>){
             
-            cout<<"  \""<<etiqueta<<"\" :[\n";
+            /*cout<<"  \""<<etiqueta<<"\*/" :[\n";
             for (size_t i=0; i< datos.size();++i){
                 cout <<"          [";
                 cout<< datos[i][0]<< "," <<datos[i][1];
@@ -62,4 +61,29 @@ public:
         }
 
     }
+};
+
+//CLASE 2
+template<typename T>
+class creadoraJson { //se pasa como atributo private la clase 1 y ahi se llama y se concatena con la etiqueta correspondiente 
+private:
+
+    generadora crear;
+
+public:
+    template<typename T>
+    void agregar_vect(const string& etiqueta,const T& datos){
+        auto gen= make_shared<generadora<T>>(); //me creo obj de clase gen
+        gen->agregar(datos);
+        vect.push_back({etiqueta, gen}); 
+    } //NO VA VA EN CLASE 1 
+
+    void printJson(){
+        cout<<"{";
+        for(size_t i=0; i< vect.size(); ++i){
+            vect[i].second->construir_Json(vect[i].first); //dereferencio para entrar al derivado de la base
+        }
+        cout<<"}\n";
+    }
+
 };
